8b1adb8499404f4984b589431c27055c788a59c5
ozank <ozankoksal@hotmail.com>
2023-05-27 23:45:45 +0300

    Added global config and table component

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,8 +1,18 @@
+'use client'
+import { API_HOST } from '@/config/appConfig'
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 export const Table = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: (): Promise<any> =>
+      fetch(`${API_HOST}/leaderboard`).then((res) => console.log(res.json()))
+  })
+
   return (
     <div>
+      <div>{data}</div>
       <div>Row 1</div>
       <div>Row 2</div>
       <div>Row 3</div>

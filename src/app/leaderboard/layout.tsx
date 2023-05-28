57f1940e38ffb6bd937665c399cf0cb671b07963
ozank <ozankoksal@hotmail.com>
2023-05-28 23:00:57 +0300

    Added react query request

--- src/app/leaderboard/layout.tsx
+++ src/app/leaderboard/layout.tsx
@@ -2,22 +2,27 @@
 import * as React from 'react'
 import { Container, Grid } from '@mui/material'
 import Appbar from '../../components/appBar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function LeaderboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Container fixed>
-      <Appbar />
-      <Container>
-        <Grid container>
-          <Grid xs>
-            <div>asdfasfd</div>
+    <QueryClientProvider client={queryClient}>
+      <Container fixed>
+        <Appbar />
+        <Container>
+          <Grid container>
+            <Grid xs>
+              <div>asdfasfd</div>
+            </Grid>
+            <Grid xs={9}>{children}</Grid>
+            <Grid xs>
+              <div>asdfasfd</div>
+            </Grid>
           </Grid>
-          <Grid xs={9}>{children}</Grid>
-          <Grid xs>
-            <div>asdfasfd</div>
-          </Grid>
-        </Grid>
+        </Container>
       </Container>
-    </Container>
+    </QueryClientProvider>
   )
 }
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,15 @@
+'use client'
 import { Table } from '@/components/table/table'
 import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 export default function Page() {
-  return <Table></Table>
+  const { data, isLoading } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: (): Promise<any> =>
+      axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/leaderboard`).then((res) => res.data)
+  })
+
+  return isLoading ? <div>Loading</div> : <Table data={data} />
 }

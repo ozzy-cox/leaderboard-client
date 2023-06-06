2a3498736b38bb839ac08d2726775a494725dbcd
ozank <ozankoksal@hotmail.com>
2023-06-06 23:42:57 +0300

    Added search request function

--- src/app/leaderboard/getLeaderboard.ts
+++ src/app/leaderboard/getLeaderboard.ts
@@ -2,3 +2,11 @@ import axios from 'axios'
 
 export const getLeaderboard = async (): Promise<any> =>
   axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/leaderboard`).then((res) => res.data)
+
+export const searchUsers = async (query: string): Promise<any> => {
+  if (query && query.length) {
+    return axios
+      .get(`${process.env.NEXT_PUBLIC_API_HOST}/search-users/?query=${query}`)
+      .then((res) => res.data)
+  }
+}
